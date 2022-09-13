41e357cffbdb5b0eb08ef81296605f44a3e5cff5
Saiful Islam <saiful1561997@gmail.com>
2022-09-14 02:39:05 +0600

    Alert Dialogue

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -21,12 +22,37 @@ class _MainPageState extends State<MainPage> {
         children: [
           Text("Login successful"),
           ElevatedButton(
-              onPressed: () async {
-                SharedPreferences pref = await SharedPreferences.getInstance();
-                await pref.clear();
-                Navigator.of(context).pushAndRemoveUntil(
-                    MaterialPageRoute(builder: ((context) => LogInPage())),
-                    (route) => false);
+              onPressed: () {
+                showCupertinoDialog(
+                    context: context,
+                    builder: (context) => CupertinoAlertDialog(
+                          content: Text(
+                            "Clicking 'yes' you will be signed out from this page! ",
+                            style: TextStyle(fontSize: 14),
+                          ),
+                          title: Text("Are you sure?"),
+                          actions: [
+                            CupertinoDialogAction(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text("No")),
+                            CupertinoDialogAction(
+                                onPressed: () async {
+                                  SharedPreferences pref =
+                                      await SharedPreferences.getInstance();
+                                  await pref.clear();
+                                  Navigator.of(context).pushAndRemoveUntil(
+                                      MaterialPageRoute(
+                                          builder: ((context) => LogInPage())),
+                                      (route) => false);
+                                },
+                                child: Text(
+                                  "Yes",
+                                  style: TextStyle(color: Colors.red),
+                                ))
+                          ],
+                        ));
               },
               child: Text("Log Out"))
         ],
